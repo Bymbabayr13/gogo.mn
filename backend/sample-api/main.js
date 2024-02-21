ffe79c88440149b629a7a651a506c2df53ee1c9b
Bymbabayr13 <151616030+Bymbabayr13@users.noreply.github.com>
2024-02-21 21:09:14 +0800

    sdfgsdg

--- backend/sample-api/main.js
+++ backend/sample-api/main.js
@@ -7,18 +7,38 @@ var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.get("/create", (req, res) => {
-  const { title, desc } = req.query;
+
+app.get("/articles", (_req, res) => {
   const data = fs.readFileSync("test.json", "utf8");
   const list = JSON.parse(data);
-  const articleid = list.length + 1;
+
+  res.json(list);
+});
+app.post("/create", (req, res) => {
+  const { title, desc } = req.body;
+  const data = fs.readFileSync("test.json", "utf8");
+  const list = JSON.parse(data);
+  const articleid = Date.now();
   list.push({
     id: articleid,
-    name: title,
-    desc: desc,
+    title: title,
   });
+
   fs.writeFileSync("test.json", JSON.stringify(list));
-  res.json({ list });
+  res.json(list);
+});
+
+app.delete("/create/delete/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const data = fs.readFileSync("test.json", "utf8");
+  const list = JSON.parse(data);
+  console.log("list");
+  const newList = list.filter((item) => item.id !== Number(id));
+  console.log("new", newList);
+
+  fs.writeFileSync("test.json", JSON.stringify(newList));
+  res.json([{ status: "Success" }]);
 });
 
 app.listen(port, () => {
