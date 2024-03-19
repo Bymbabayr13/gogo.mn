28a6d27ad4937d75ead58e0d739579c97de82306
Bymbabayr13 <151616030+Bymbabayr13@users.noreply.github.com>
2024-03-19 18:52:34 +0800

    asdf

--- type_sample/frontend/typescript/pages/index.tsx
+++ type_sample/frontend/typescript/pages/index.tsx
@@ -1,31 +1,26 @@
-
 import { Inter } from "next/font/google";
-import { title } from "process";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  type post = {
+    black: string;
+    title: string;
+  };
+  type card = {
+    name: string;
+    title: string;
+    posts: post[];
+  };
 
-
-type post ={
-  black:string;
-  title:string;
-}
-type card ={
-  name :string;
-  title:string;
-  posts:post[]
-}
-
-const  card =(props:card) => {
-    const {name , title , posts} = props
+  const card = (props: card) => {
     return (
       <div>
-      <h1>{title}</h1>
+        <h1>{props.name}</h1>
+        <h1>{props.title}</h1>
+        <h1>{props.posts[1].black}</h1>
+        asdfas
       </div>
-        );
-}
-
-
-
+    );
+  };
 }
